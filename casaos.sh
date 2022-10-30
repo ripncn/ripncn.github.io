e446cbcbdbda078eba2410390c499f466ed907e7
Ripncn <60642255+ripncn@users.noreply.github.com>
2022-10-30 20:37:38 +0800

    Update casaos.sh

--- casaos.sh
+++ casaos.sh
@@ -71,9 +71,9 @@ readonly CASA_DB_PATH=/var/lib/casaos/db/
 readonly CASA_TEMP_PATH=/var/lib/casaos/temp/
 readonly CASA_LOGS_PATH=/var/log/casaos/
 readonly CASA_PACKAGE_EXT=".tar.gz"
-readonly CASA_RELEASE_API="https://api.github.com/repos/${CASA_REPO}/releases"
-readonly CASA_OPENWRT_DOCS="https://git.histb.com/IceWhaleTech/CasaOS-OpenWrt"
-readonly CASA_UNINSTALL_URL="https://raw.hisi.ga/IceWhaleTech/get/main/casaos-uninstall"
+readonly CASA_RELEASE_API="https://hub.lovou.eu.org/https://api.github.com/repos/${CASA_REPO}/releases"
+readonly CASA_OPENWRT_DOCS="http://hub.lovou.eu.org/https://github.com/IceWhaleTech/CasaOS-OpenWrt"
+readonly CASA_UNINSTALL_URL="https://hub.lovou.eu.org/https://raw.githubusercontent.com/IceWhaleTech/get/main/casaos-uninstall"
 readonly CASA_VERSION_URL="https://api.casaos.io/casaos-api/version"
 readonly CASA_UNINSTALL_PATH=/usr/bin/casaos-uninstall
 
@@ -479,7 +479,7 @@ Download_CasaOS() {
     else
         Casa_Tag="$version"
     fi
-    Casa_Package_URL="https://git.histb.com/${CASA_REPO}/releases/download/${Casa_Tag}/${Casa_Package}"
+    Casa_Package_URL="https://hub.lovou.eu.org/https://github.com/${CASA_REPO}/releases/download/${Casa_Tag}/${Casa_Package}"
     # Remove Temp File
     ${sudo_cmd} rm -rf "$PREFIX/tmp/${Casa_Package}"
     # Download Package
